61782bdeab84ed82f86bcfe85e974edfc92cc9fa
Jan Hovland <jan.hovland@lyse.net>
2021-08-13 11:46:01 +0200

    Update

--- PrivateDatabase/ViewModel.swift
+++ PrivateDatabase/ViewModel.swift
@@ -5,6 +5,17 @@
 
 import os.log
 import CloudKit
+import OSLog
+import Foundation
+
+
+struct Contact: Identifiable {
+    let id: String
+    let name: String
+    let phoneNumber: String
+    let associatedRecord: CKRecord
+}
+
 
 class ViewModel: ObservableObject {
 
@@ -51,14 +62,22 @@ class ViewModel: ObservableObject {
         // This completion block will execute once for every record saved. In this sample,
         // we will only ever be saving a single record, so we only expect this to get called
         // once.
-        saveOperation.perRecordCompletionBlock = { record, error in
-            os_log("Record with ID \(record.recordID.recordName) was saved.")
 
-            if let error = error {
-                self.reportError(error)
+//        saveOperation.perRecordCompletionBlock = { record, error in
+        saveOperation.modifyRecordsResultBlock = { [weak self] result in
+//            os_log("Record with ID \(record.recordID.recordName) was saved.")
+            
+            DispatchQueue.main.async {
+                switch result {
+                case .failure(let error):
+                    print(error)
+                    
+                case .success:
+                    print("Saved OK")
+                }
             }
 
-            self.getLastPerson()
+            self?.getLastPerson()
         }
 
         // This completion block will execute once when the entire CKModifyRecordsOperation
@@ -67,17 +86,17 @@ class ViewModel: ObservableObject {
         // in this sample and simply parsed the array of records passed in, of which there would
         // only have been one in this particular sample. In this sample, we are using both for
         // illustrative purposes.
-        saveOperation.modifyRecordsCompletionBlock = { _, _, error in
-            if let error = error {
-                self.reportError(error)
-
-                // If a completion was supplied, pass along the error here.
-                completionHandler?(.failure(error))
-            } else {
-                // If a completion was supplied, like during tests, call it back now.
-                completionHandler?(.success(()))
-            }
-        }
+//        saveOperation.modifyRecordsCompletionBlock = { _, _, error in
+//            if let error = error {
+//                self.reportError(error)
+//
+//                // If a completion was supplied, pass along the error here.
+//                completionHandler?(.failure(error))
+//            } else {
+//                // If a completion was supplied, like during tests, call it back now.
+//                completionHandler?(.success(()))
+//            }
+//        }
 
         database.add(saveOperation)
     }
@@ -162,6 +181,76 @@ class ViewModel: ObservableObject {
 
         default:
             os_log("CKError: \(error.localizedDescription)")
+        }
+    }
+    
+    
+//    /// Adds a new Contact to the database, using `encryptedValues` to encrypt the Contact's phone number.
+//    /// - Parameters:
+//    ///   - name: Name of the Contact.
+//    ///   - phoneNumber: Phone number of the contact which will be stored in an encrypted field.
+//    ///   - completionHandler: Handler to process success or failure of the operation.
+//    func addContact(
+//        name: String,
+//        phoneNumber: String,
+//        completionHandler: @escaping (Result<Contact?, Error>) -> Void
+//    ) {
+//        //        let record = CKRecord(recordType: "Contact", recordID: CKRecord.ID(zoneID: recordZone.zoneID))
+//        let record = CKRecord(recordType: "Contact", recordID: lastPersonRecordID)
+//        record["name"] = name
+//        record.encryptedValues["phoneNumber"] = phoneNumber
+//
+//        let saveOperation = CKModifyRecordsOperation(recordsToSave: [record])
+//        saveOperation.savePolicy = .allKeys
+//
+//        saveOperation.modifyRecordsResultBlock = { [weak self] result in
+//
+//            DispatchQueue.main.async {
+//                switch result {
+//                case .failure(let error):
+//                    self?.handleError(error)
+//                    print(error)
+//                    completionHandler(.failure(error))
+//
+//                case .success:
+//                    let contact = Contact(record: record)
+//                    completionHandler(.success(contact))
+//                    print("Saved OK")
+//                }
+//            }
+//        }
+//
+//        database.add(saveOperation)
+//    }
+
+    private func handleError(_ error: Error) {
+        guard let ckerror = error as? CKError else {
+            os_log("Not a CKError: \(error.localizedDescription)")
+            return
+        }
+
+        switch ckerror.code {
+        case .zoneNotFound:
+            if ckerror.userInfo[CKErrorUserDidResetEncryptedDataKey] != nil {
+                // CloudKit is unable to decrypt previously encrypted data. This occurs when a user
+                // resets their iCloud Keychain and thus deletes the key material previously used
+                // to encrypt and decrypt their encrypted fields stored via CloudKit.
+                // In this case, it is recommended to delete the associated zone and re-upload any
+                // locally cached data, which will be encrypted with the new key.
+                os_log("Encryption key has been reset by user.")
+            }
+
+        case .partialFailure:
+            // Iterate through error(s) in partial failure and report each one.
+            let dict = ckerror.userInfo[CKPartialErrorsByItemIDKey] as? [NSObject: CKError]
+            if let errorDictionary = dict {
+                for (_, error) in errorDictionary {
+                    os_log("An error occurred: \(error.localizedDescription)")
+                }
+            }
+
+        default:
+            os_log("CKError: Code \(ckerror.code.rawValue): \(ckerror.localizedDescription)")
         }
     }
 
